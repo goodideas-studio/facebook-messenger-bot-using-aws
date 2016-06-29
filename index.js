9d77dfec07f3650d41737d31f3102eef60edb33c
jacky810124 <jacky810124@hotmail.com>
2016-06-30 05:20:42 +0800

    feat: Reply message when user's message include specific keywords.

--- index.js
+++ index.js
@@ -2,27 +2,41 @@
 
 const fetch = require('node-fetch')
 
-const PAGE_ACCESS_TOKEN = `PUT_YOUR_TOKEN_HERE`
+const PAGE_ACCESS_TOKEN = `PUT_YOUR_PAGE_ACCESS_TOKEN_HERE`
 
 /**
  * Main function
  */
 exports.replyMessage = (event, context, callback) => {
 
-  const MESSAGE_BODY = processMessageFromSender(event)
 
-  sendMessageToSender(MESSAGE_BODY)
-    .then(response => response.json())
-    .then(result => {
+    const MESSAGE_BODY = processMessageFromSender(event)
+    const SENDER_INFORMATION = { name: '' }
 
-      callback(null, result)
+    getSenderInformation(MESSAGE_BODY.recipient.id)
+      .then(response => response.json())
+      .then(snederInformation => {
 
-    })
-    .catch(error => {
+        const text = replyMessageWithSpecificKeywords(MESSAGE_BODY.message.text)
 
-      callback(error)
+        SENDER_INFORMATION.name = `${snederInformation.first_name}`
+        MESSAGE_BODY.message.text = `${SENDER_INFORMATION.name}, ${text}`
 
-    })
+        return sendMessageToSender(MESSAGE_BODY)
+
+      })
+      .then(response => response.json())
+      .then(result => {
+
+        callback(null, result)
+
+      })
+      .catch(error => {
+
+        callback(error)
+
+      })
+
 }
 
 /**
@@ -77,13 +91,32 @@ exports.replyMessage = (event, context, callback) => {
  */
 const replyMessageWithSpecificKeywords = (messageText) => {
 
+  if (messageText.indexOf('portfolio') >= 0) {
+
+    return `Hi nice to meet you! here is my GitHub url: https://github.com/jacky810124`
+
+  } else if (messageText.indexOf('contact') >= 0) {
+
+    return `Hi nice to meet you! my email is: jacky810124@hotmail.com \n\nor here is my facebook: https:facebook.com/huangjikang`
+
+  } else if (messageText.indexOf('help') >= 0) {
+
+    return `You can type 'portfolio' or 'contact' to get more information.`
+
+  } else {
+
+    return `Sorry, I just a bot, I can not understand what are you talking about. You can type 'help' to get more information!`
+
+  }
 
 }
 
 /**
  * get user information
  */
-const getSenderInformation = (senderId) => {
 
+ const getSenderInformation = (senderId) => {
 
-}
+   return fetch(`https://graph.facebook.com/v2.6/${senderId}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${PAGE_ACCESS_TOKEN}`)
+
+ }
